49db95ec74177f971b33d0fce7adaf041fb20afc
Petrik Diak <diak@petrik.hu>
2026-02-11 09:37:13 +0100

    2

--- RPGGameWPF/MainWindow.xaml.cs
+++ RPGGameWPF/MainWindow.xaml.cs
@@ -73,7 +73,7 @@ namespace RPGGameWPF
 			
 			var hero = new Hero(cls, new Position(1, 1));
 			state = new GameState(map, hero);
-
+			 
 			SpawnEnemies(state, seed);
 			SpawnChestAndDrops(state, seed);
 			BuildMapVisual(); 
